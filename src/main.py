ac8b0b046a42653efc01eea5e2405ad65bd49aea
Filip Myslinski <filipmyslinski2006@gmail.com>
2026-01-07 20:16:54 +0100

    feat: REPL

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from lang.token import Token
 
 
 class Args:
-    filename: str = ""
+    filename: str | None = None
     verbose: bool = False
 
 
@@ -15,25 +15,40 @@ def main():
     parser = argparse.ArgumentParser(
         prog="waycro", description="Macro tool and language for Wayland Compositors"
     )
-    _ = parser.add_argument("filename", type=str)
+    _ = parser.add_argument("filename", type=str, nargs="?")
     _ = parser.add_argument(
         "-v", "--verbose", action="store_true", help="enable verbose logging"
     )
 
     args = parser.parse_args(namespace=Args())
-    with open(args.filename, "r") as f:
-        txt = f.read()
-        lexer = Lexer(txt, args.filename)
-        toks, err = lexer.tokenize()
-        if err:
-            print(err)
-        else:
-            parser = Parser(cast(list[Token], toks))
-            res = parser.parse()
-            if res.error:
-                print(res.error)
+    if args.filename:
+        with open(args.filename, "r") as f:
+            txt = f.read()
+            lexer = Lexer(txt, args.filename)
+            toks, err = lexer.tokenize()
+            if err:
+                print(err)
             else:
-                print(res.result)
+                parser = Parser(cast(list[Token], toks))
+                res = parser.parse()
+                if res.error:
+                    print(res.error)
+                else:
+                    print(res.result)
+    else:  # REPL
+        while True:
+            prompt = input("waycro REPL> ")
+            lexer = Lexer(prompt, "<REPL>")
+            toks, err = lexer.tokenize()
+            if err:
+                print(err)
+            else:
+                parser = Parser(cast(list[Token], toks))
+                res = parser.parse()
+                if res.error:
+                    print(res.error)
+                else:
+                    print(res.result)
 
 
 if __name__ == "__main__":
